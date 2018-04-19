ead486611980d5a89fc455edf63f38fec0667e00
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-04-19 12:22:58 -0300

    vectores paralelos, funcion
    vectores paralelos con funciones de calcular promedio y mostrar alumnos

---
+++ Clase_07/vectoresParalelos/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//funcion que reciba vector de legajos y que reciba un numero de legajo. va a devolver el indice del legajo o va a devolver -1
+
+float calcularPromedio(int,int);
+void mostrarAlumnos(int[], char[][30], int, int, float, int);//al tipo char le indico tambien que tiene un array de 30 bits.
+
+int main()
+{
+    int legajos[3];// todos los vectores tienen que tener la misma capacidad para poder almacenar datos en cada uno
+    char nombres[3][30];//pongo una segunda dimension que indique cuantos caracteres limites pueden tener. ahi puedo ingresar 3 nombres con un limite de 29 caracteres.
+    int nota1[3];
+    int nota2[3];
+    float promedio[3];
+    int i;
+
+    for(i=0;i<3;i++)
+    {
+        printf("Ingrese un legajo: ");
+        scanf("%d", &legajos[i]);
+
+        printf("\nIngrese nombre: ");
+        fflush(stdin);
+        gets(nombres[i]);//le paso i asi se guarda en ese espacio del vector.
+
+        printf("\nIngrese nota 1: ");
+        scanf("%d", &nota1[i]);
+
+        printf("\nIngrese nota 2: ");
+        scanf("%d", &nota2[i]);
+
+
+        promedio[i]=calcularPromedio(nota1[i],nota2[i]);
+
+        //printf("\nEl promedio es de: %2.f",promedio[i]);
+
+        printf("\n\n\n");
+    }
+
+    mostrarAlumnos(legajos,nombres,nota1,nota2);//le paso los valores a la funcion para que los muestre.
+    /*
+    for(i=0;i<3;i++)
+    {
+        printf("Legajo:%d\n Nombre:%s\n Nota1:%d\n Nota2:%d\n Pormedio:%.2f\n", legajos[i],nombres[i],nota1[i],nota2[i],promedio[i]);
+    }
+    */
+    return 0;
+}
+
+//funcion calcular promedio:
+
+float calcularPromedio(int nota1, int nota2)
+{
+    //no usar for porque no le paso los vectores, le paso los valores directamente y lo calcula en el for del main.
+    float promedio;
+
+    promedio=(float)(nota1+nota2)/2;//casteamos la sumatoria de nota1 y nota2.
+
+    return promedio;
+}
+
+//funcion mostrarAlumnos:
+
+void mostrarAlumnos(int legajo[],char nombres[][30], int nota1[], int nota2[], float promedio[], int cantidad)// la funcion tiene que mostrar los valores del vector, entonces se lo tengo que pasar.
+{
+    int i;
+
+    for(i=0;i<3)//hago que recorra el vector para mostrar los valores.
+    {
+
+    }
+
+}
