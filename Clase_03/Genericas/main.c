c236d195655e4d44fb8dbc8e02e2877122a12ce5
nahuel812 <36241474+nahuel812@users.noreply.github.com>
2018-08-30 16:32:31 -0300

    funciones
    pedir numero entero, float y char(incompleto)

--- Clase_03/Genericas/main.c
+++ Clase_03/Genericas/main.c
@@ -4,17 +4,29 @@
 //prototipo de la funcion
 int pedirNumeroEntero(char[]);//recibe una cadena de caracteres.
 
-float pedirNumeroFlotante();
+float pedirNumeroFlotante(char[]);
 
-char pedirNumeroFlotante();
+char pedirChar(char[]);
 
 int main()
 {
     int legajo,nota,edad;
+    float sueldo;
+    char letraInicialDelNombre;
 
-    legajo = pedirNumero("Ingrese un legajo:");//llamada de la funcion.
-    nota = pedirNumero("Ingrese una nota:");//Le paso el texto que quiero que mustre a la funcion.
-    edad = pedirNumero("Ingrese una edad:");
+    legajo = pedirNumeroEntero("Ingrese un legajo:");//llamada de la funcion.
+    nota = pedirNumeroEntero("Ingrese una nota:");//Le paso el texto que quiero que mustre a la funcion.
+    edad = pedirNumeroEntero("Ingrese una edad:");
+
+    sueldo = pedirNumeroFlotante("Ingrese su sueldo: ");
+
+    letraInicialDelNombre = pedirChar("Ingrese la primera letra de su nombre: ");
+
+    printf("\nlegajo: %d",legajo);
+    printf("\nnota: %d",nota);
+    printf("\nedad: %d",edad);
+    printf("\nsueldo: %f",sueldo);
+    //printf("letra inicial del nombre: %c",letraInicialDelNombre);
 
     return 0;
 }
@@ -30,4 +42,27 @@ int pedirNumeroEntero(char texto[])//Esta funcion me permite pedir numeros y pod
     scanf("%d", &numero);
 
     return numero;
+}
+
+float pedirNumeroFlotante(char texto[])//Esta funcion me permite pedir numeros y poder modificar el texto de lo que pide.
+{
+    float numero;
+
+    printf("%s",texto);//la mascara que me permite mostrar string es "%s"
+
+    scanf("%f", &numero);
+
+    return numero;
+}
+
+char pedirChar(char texto[])
+{
+    char letra;
+
+    fflush(stdin);
+    printf("%s",texto);//la mascara que me permite mostrar string es "%s"
+
+    scanf("%c", letra);
+
+    return letra;
 }
